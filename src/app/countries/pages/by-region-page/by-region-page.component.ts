504de89bf21d2b8147beda13e99c732379c21bde
Luis <luisalarcon1992@gmail.com>
2024-02-29 23:05:49 -0300

    añadiendo states

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { CountriesService } from '../../services/countries.service';
 import { ICountry } from '../../interfaces/country';
 
+type Region = 'Africa'|'Americas'|'Europe'|'Asia'|'Oceania';
+
 @Component({
   selector: 'country-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -10,11 +12,17 @@ import { ICountry } from '../../interfaces/country';
 })
 export class ByRegionPageComponent {
 
-  constructor( private countriesService: CountriesService){}
+  constructor( private countriesService: CountriesService){};
 
   public region: ICountry[] = [];
 
-  searchByRegion( region: string ) {
+  public regions?: Region[] = ['Africa','Americas','Europe','Asia','Oceania'];
+
+  public selectedRegion?: Region;
+
+  searchByRegion( region: Region ) {
+
+    this.selectedRegion = region;
     this.countriesService.searchRegion(region).subscribe(
       regions => this.region = regions
     )
